74e8793eac4942210851e10c5c66291d00cd572c
lucastamura <lucas10.tamura@gmail.com>
2023-07-05 10:27:10 -0300

    fix: casa de vidro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Site/home/home.component';
+import { CasaVidroComponent } from './Site/casa-vidro/casa-vidro.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'casa-vidro', component: CasaVidroComponent }
 ];
 
 @NgModule({
