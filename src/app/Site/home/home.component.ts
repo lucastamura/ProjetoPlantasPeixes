54b1c4b294dd091194f6f67fb5e19b3e81e1a8dd
lucastamura <lucas10.tamura@gmail.com>
2023-07-05 10:02:05 -0300

    feat: home

--- src/app/Site/home/home.component.ts
+++ src/app/Site/home/home.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  text1 = 'Tudo começou no fundo do quintal, em um projeto caseiro de aquaponia feito com materiais reciclados. Inspirado pelo desejo de se reconectar com a natureza, nosso fundador, Coelho, embarcou em uma jornada de descobertas e aprendizados.';
 
+  text2 = 'A aquaponia se revelou uma solução fascinante, combinando o cultivo de plantas com a criação de peixes em um sistema simbiótico. O quintal se transformou em um laboratório vivo, onde os princípios da sustentabilidade e da autossuficiência foram colocados em prática.';
+
+  text3 = 'À medida que a paixão de Coelho pela aquaponia crescia, o projeto também expandia. Hoje, orgulhamo-nos de oferecer um espaço aberto ao público, onde a aquaponia profissional é o centro das atenções. Nosso objetivo é compartilhar conhecimento e proporcionar experiências enriquecedoras para todos os visitantes.'
+
+  text4 = 'Ao explorar nossas instalações, você encontrará um ambiente diversificado e acolhedor. Além do sistema de aquaponia, temos salas de aula equipadas para aulas  interativas. Aqui, você pode aprender técnicas avançadas de cultivo, entender a importância da sustentabilidade e descobrir como trazer a natureza para dentro de sua própria casa.'
+
+  text5 = 'À medida que a paixão de Coelho pela aquaponia crescia, o projeto também expandia. Hoje, orgulhamo-nos de oferecer um espaço aberto ao público, onde a aquaponia profissional é o centro das atenções. Nosso objetivo é compartilhar conhecimento e proporcionar experiências enriquecedoras para todos os visitantes.'
+
+  text6 = 'Ao explorar nossas instalações, você encontrará um ambiente diversificado e acolhedor. Além do sistema de aquaponia, temos salas de aula equipadas para aulas e workshops interativos. Aqui, você pode aprender técnicas avançadas de cultivo, entender a importância da sustentabilidade e descobrir como trazer a natureza para dentro de sua própria casa.'
+
+  text7 = 'Explore nosso site, visite nosso espaço e faça parte da nossa comunidade comprometida em promover a harmonia entre plantas, animais e seres humanos. Juntos, podemos criar um futuro mais equilibrado e vibrante.'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Site/home/home.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
