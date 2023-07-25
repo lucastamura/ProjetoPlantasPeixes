2dac99e73ace37a3a804462707c792b3ad4a8f17
lucastamura <lucas10.tamura@gmail.com>
2023-07-25 11:38:00 -0300

    add: casa de vidro

--- src/app/Site/casa-vidro/casa-vidro.component.ts
+++ src/app/Site/casa-vidro/casa-vidro.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./casa-vidro.component.scss']
 })
 export class CasaVidroComponent {
+  text1 = "Imagine uma sala de aula única, onde as paredes brilham com a luz suave filtrada através de garrafas de vidro colorido. Bem-vindo à Casa de Vidro, um projeto visionário que busca criar um espaço de aprendizado inspirador e sustentável."
 
+  text2 = "Nossa visão é construir uma sala de aula inovadora, com as paredes feitas inteiramente de garrafas de vidro recicladas. A transparência e a beleza do vidro nos permitirão desfrutar da luz natural, conectando-nos à natureza enquanto aprendemos e exploramos novas ideias."
+
+  text3 = "Ao participar deste projeto, você estará contribuindo para um futuro sustentável, onde a educação e a consciência ambiental caminham lado a lado. A Casa de Vidro será mais do que apenas uma sala de aula, será um símbolo de inovação, reciclagem e conexão com a natureza."
+
+  text4 = "Junte-se a nós na construção dessa jornada. Traga suas garrafas de vidro verde e seu entusiasmo para ajudar a criar um espaço único que inspire gerações futuras. Vamos trabalhar juntos para construir a Casa de Vidro, um marco de sustentabilidade e aprendizado para a nossa comunidade."
+
+  text5 = "Para tornar esse projeto uma realidade, convidamos você a fazer parte desta jornada transformadora. Estamos em busca de garrafas de vidro verde, que serão cuidadosamente selecionadas e integradas às paredes da Casa de Vidro. Se você tem garrafas de vidro vazias e deseja contribuir para a criação de um espaço educacional único, junte-se a nós!"
+
+  text6 = "Além disso, estamos buscando voluntários dedicados e apaixonados para nos ajudar na construção da Casa de Vidro. Se você é habilidoso com ferramentas, tem experiência em construção ou simplesmente quer fazer parte de algo especial, seu apoio é essencial para tornar essa visão uma realidade tangível."
+
+  text7 = "Entre em contato conosco para mais informações sobre como contribuir com garrafas de vidro ou se voluntariar na construção da Casa de Vidro. Juntos, podemos fazer a diferença e construir um futuro mais brilhante para todos."
+
+  text8 = "Seja parte desta história. Seja parte da Casa de Vidro."
 }
